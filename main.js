3c064fd53209737439f38b4b214b71928ef82096
Anton-Ymous <108355338+Anton-Ymous@users.noreply.github.com>
2022-08-08 23:45:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 fetch("test.json")
   .then(response => response.json())
-  .then(json => console.log(json));
+  .then(json => console.log(json))
 
 
 document.querySelector("button").addEventListener("click", ()=>{
