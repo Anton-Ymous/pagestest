171d94b5e92b864d2ce002fd6f54db7c7bdba9c6
Antonymous <alternativebeginning@gmail.com>
2022-08-08 23:34:53 -0500

    test

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var mydata = JSON.parse(data);
-console.log(mydata);
+fetch("test.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
 
 document.querySelector("button").addEventListener("click", ()=>{
     let newElem = document.createElement("div");
